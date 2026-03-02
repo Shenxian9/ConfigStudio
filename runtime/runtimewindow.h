0f24a8c0736f0bab1024ccba372424d0b54dd7db
Shxi9 <145898128+Shenxian9@users.noreply.github.com>
2026-03-02 17:02:19 +0800

    fix: lock canvas item interaction in fullscreen runtime mode

--- runtime/runtimewindow.h
+++ runtime/runtimewindow.h
@@ -49,6 +49,7 @@ private:
     QSizePolicy m_originalPolicy;
 
      QMap<CanvasItem*, QRect> m_originalItemGeometries;
+     QMap<CanvasItem*, bool> m_originalTransparentForMouseEvents;
 
       QVector<int> m_originalStretchs; // 保存父布局所有 widget 的 stretch
 
