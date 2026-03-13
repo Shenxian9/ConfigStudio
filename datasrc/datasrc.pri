c4b52a662fd42b67237a3299530e4ba0808eb2c8
Shxi9 <145898128+Shenxian9@users.noreply.github.com>
2026-03-13 16:06:29 +0800

    Add serial data source framework for /dev/ttyS2 ingestion

--- datasrc/datasrc.pri
+++ datasrc/datasrc.pri
@@ -2,7 +2,9 @@ FORMS += \
     $$PWD/datasrc.ui
 
 HEADERS += \
-    $$PWD/datasrc.h
+    $$PWD/datasrc.h \
+    $$PWD/serialdatasource.h
 
 SOURCES += \
-    $$PWD/datasrc.cpp
+    $$PWD/datasrc.cpp \
+    $$PWD/serialdatasource.cpp
---
+++ datasrc/serialdatasource.h
@@ -0,0 +1,75 @@
+#ifndef SERIALDATASOURCE_H
+#define SERIALDATASOURCE_H
+
+#pragma once
+
+#include <QObject>
+#include <QByteArray>
+#include <QHash>
+#include <QSerialPort>
+
+class VariableModel;
+
+struct SerialPortConfig {
+    QString portName = QStringLiteral("/dev/ttyS2");
+    qint32 baudRate = QSerialPort::Baud9600;
+    QSerialPort::DataBits dataBits = QSerialPort::Data8;
+    QSerialPort::Parity parity = QSerialPort::NoParity;
+    QSerialPort::StopBits stopBits = QSerialPort::OneStop;
+    QSerialPort::FlowControl flowControl = QSerialPort::NoFlowControl;
+    QByteArray frameTerminator = "\n";
+};
+
+class SerialDataSource : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SerialDataSource(QObject *parent = nullptr);
+
+    void setConfig(const SerialPortConfig &config);
+    SerialPortConfig config() const { return m_config; }
+
+    bool open();
+    void close();
+    bool isOpen() const;
+
+signals:
+    void frameReceived(const QByteArray &frame);
+    void rawBytesReceived(const QByteArray &bytes);
+    void statusChanged(bool opened);
+    void errorOccurred(const QString &errorText);
+
+private slots:
+    void onReadyRead();
+    void onSerialError(QSerialPort::SerialPortError err);
+
+private:
+    void applyPortConfig();
+    void extractFrames();
+
+    QSerialPort m_serial;
+    SerialPortConfig m_config;
+    QByteArray m_rxBuffer;
+};
+
+class SerialVariableMapper : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SerialVariableMapper(VariableModel *model, QObject *parent = nullptr);
+
+    void setBinding(const QString &sourceKey, const QString &varId);
+    void removeBinding(const QString &sourceKey);
+    void clearBindings();
+
+public slots:
+    void onFrameReceived(const QByteArray &frame);
+
+private:
+    void processKeyValue(const QString &key, const QString &rawValue);
+
+    VariableModel *m_model = nullptr;
+    QHash<QString, QString> m_keyToVarId;
+};
+
+#endif // SERIALDATASOURCE_H
